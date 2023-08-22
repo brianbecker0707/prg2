46bd89671601facc18a7c777514621c4e1ba5068
aluno <brianbecker0707@gmail.com>
2023-08-22 14:58:08 -0300

    da certo, por favor

---
+++ cmake-build-debug/TarefaLoginUsuario.cpp
@@ -0,0 +1,11 @@
+// Created by aluno on 22/08/23.
+
+#include <stdio.h>
+
+int main (){
+
+    printf("hell porra");
+
+
+    return 0;
+}
---
+++ cmake-build-debug/TarefaLoginUsuario.h
@@ -0,0 +1,14 @@
+//
+// Created by aluno on 22/08/23.
+//
+
+#ifndef PRG2_TAREFALOGINUSUARIO_H
+#define PRG2_TAREFALOGINUSUARIO_H
+
+
+class TarefaLoginUsuario {
+
+};
+
+
+#endif //PRG2_TAREFALOGINUSUARIO_H
